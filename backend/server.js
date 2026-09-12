439f9a8e6846503d55b9464310a78a49543ca22e
gurralavarun <gurrala.varun11@gmail.com>
2026-09-12 15:08:54 +0530

    added api

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/api', (req, res) => {
+  res.json({ message: 'This is the API endpoint' });
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
